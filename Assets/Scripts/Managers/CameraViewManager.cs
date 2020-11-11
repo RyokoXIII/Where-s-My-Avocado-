d5c7196761cfdf88aa2bba84051bec49c206b9cc
RyokoXIII <mizuho19989@gmail.com>
2020-11-11 15:04:16 +0700

    Add CameraViewManager script
    add script to Main Camera and add to prefab

---
+++ Assets/Scripts/Managers/CameraViewManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraViewManager : MonoBehaviour
+{
+    [SerializeField]
+    Camera _camera;
+    public SpriteRenderer zoom;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        _camera = GetComponent<Camera>();
+
+        float screenRatio = (float)Screen.width / (float)Screen.height;
+        float targetRatio = zoom.bounds.size.x / zoom.bounds.size.y;
+
+        if (screenRatio >= targetRatio)
+        {
+            _camera.orthographicSize = zoom.bounds.size.y / 2;
+        }
+        else
+        {
+            float differenceInSize = targetRatio / screenRatio;
+            _camera.orthographicSize = zoom.bounds.size.y / 2 * differenceInSize;
+        }
+    }
+}
